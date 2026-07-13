fcac5102228b68efda7299ca9a6e9ea46753782f
dharmapalbharati-web <dharmapalbharati2525@gmail.com>
2026-07-13 18:50:33 +0530

    Remove Chatapp, Chats, Login, and Register components along with their associated styles and API service

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,7 @@
-const connectDB = require('./src/config/db');
-
-const { app, server } = require('./src/app');
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,34 +1,5 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
-const authRoutes = require('./Routers/auth.router');
-
-dotenv.config();
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true,
-  },
-});
 
-// CORS Middleware
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
-app.use(express.json());
-app.use(cookieParser());
-
-// Routes
-app.use('/api', authRoutes);
-
-module.exports = { app, server, io };
+module.exports = app
